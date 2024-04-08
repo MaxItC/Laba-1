b3ec18a1b445b047faaac98d621402badd535d37
Max Kaplunov <150555193+MaxItC@users.noreply.github.com>
2024-04-08 23:03:04 +0300

    Add files via upload

---
+++ Laba-1.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Project
+{
+    static void Main(string[] args)
+    {
+        List<Obj> list = new List<Obj>
+        {
+            new Obj {Id = 135},
+            new Obj {Id = 142},
+            new Obj {Id = 354},
+            new Obj {Id = 176},
+            new Obj {Id = 999},
+            new Obj {Id = 777},
+        };
+
+        // шукаемо за властивостью ( э цифра 7)
+        var founded = list.Where(x => x.Id.ToString().Contains("7")); // LINQ
+
+        
+        foreach(var i in founded)
+         Console.WriteLine(i.Id);
+    }
+
+    class Obj
+    {
+        public int Id;
+    }
+}
