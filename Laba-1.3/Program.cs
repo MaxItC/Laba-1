7136ee98a8c73e9836a3b31e0cfce4fdcb38c0e8
Max Kaplunov <150555193+MaxItC@users.noreply.github.com>
2024-04-08 23:04:40 +0300

    Add files via upload

---
+++ Laba-1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Project
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>
+        {
+            1,2,3,1,4,5,2,2,1
+        };
+
+        var filtred = list.Select((el, ind) => list.Take(ind).Contains(el) ? 0 : el); // ind с 0
+
+
+          foreach (var i in filtred)
+            Console.WriteLine(i);
+
+      
+    }
+}
