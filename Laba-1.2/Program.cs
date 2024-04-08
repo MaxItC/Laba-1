34c339de983b5fa6f702dacfb10b0e8dc01212c6
Max Kaplunov <150555193+MaxItC@users.noreply.github.com>
2024-04-08 23:03:49 +0300

    Add files via upload

---
+++ Laba-1.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json;
+
+class Project
+{
+    static void Main(string[] args)
+    {
+        var dict = new Dictionary<int, string>
+        {                                                                 
+            {1,"1" },
+            {2,"1" },
+            {3,"2" },
+            {4,"4" },
+            {5,"1" },
+        };
+
+        var dict2 = new Dictionary<int, string>();
+        int count = 0;
+
+
+        for (int i = 0; i < dict.Count; i++)
+        {
+            if (!dict2.ContainsValue(dict.ElementAt(i).Value))
+            {
+                count++; // key
+                dict2.Add(count, dict.ElementAt(i).Value);
+            }
+        }
+
+        dict.Clear();
+        dict = dict2;
+
+        dict2 = null;
+        
+        foreach (KeyValuePair<int,string> i in dict)
+        {
+            Console.WriteLine("Key:{0} , Value:{1}",i.Key,i.Value);
+        }
+
+        string ser = JsonConvert.SerializeObject(dict);
+    }
+}
